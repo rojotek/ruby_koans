9d7a76a030f6a29658e8994425a63670a5147b45
Rob Dawson <robert@rojotek.com>
2011-09-23 12:50:45 +1000

    Triangle exceptions.

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -13,10 +13,12 @@
 # and
 #   about_triangle_project_2.rb
 #
-def triangle(a, b, c)
-  if a==b &&a==c &&b==c
+def triangle(a, b, c)  
+  raise TriangleError if a<=0||b<=0||c<=0
+  raise TriangleError if (a+b <= c) || (a+c <= b) || (b+c <= a)
+  if a==b && a==c && b==c
     :equilateral 
-  elsif  a==b||a==c||b==c
+  elsif  a==b || a==c || b==c
     :isosceles 
   else
     :scalene
